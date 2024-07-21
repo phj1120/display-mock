0000ff6d10805d7da75f14025f32c3aedc81f504
phj1120 <hyeonj1998@naver.com>
2024-07-22 00:32:01 +0900

    feat: 탭 검색 개발

--- api/src/test/java/com/displayapi/displayapi/repository/goods/GoodsGroupByTest.java
+++ api/src/test/java/com/displayapi/displayapi/repository/goods/GoodsGroupByTest.java
@@ -73,30 +73,9 @@ class GoodsGroupByTest {
         log.info("goodsCountDtos: {}", goodsCountDtos);
     }
 
-    private static GoodsTabSearch makeGoodsTabSearch() {
-        GoodsTabSearch goodsTabSearch = new GoodsTabSearch();
-        goodsTabSearch.setTabSearchType(TabSearchType.LUMP_SUM_PAY_GOODS.getCode());
-        goodsTabSearch.setGoodsTabList(List.of(
-                GoodsTab.of("1-1", 1L, 0L, 10000L, null),
-                GoodsTab.of("1-2", 1L, 10000L, 20000L, null),
-                GoodsTab.of("1-3", 1L, 20000L, null, null),
-
-                GoodsTab.of("2-1", 2L, 0L, 10000L, null),
-                GoodsTab.of("2-2", 2L, 10000L, 20000L, null),
-                GoodsTab.of("2-3", 2L, 20000L, null, null),
-
-                GoodsTab.of("3-1", 3L, 0L, 10000L, null),
-                GoodsTab.of("3-2", 3L, 10000L, 20000L, null),
-                GoodsTab.of("3-3", 3L, 20000L, null, null)
-        ));
-        return goodsTabSearch;
-    }
-
     @Test
     void groupByWithCaseLumpSumPayGoodsTab() {
         // 일시불: 카테고리, 가격 범위
-        makeGoodsTabSearch();
-
         StringExpression caseBuilder = new CaseBuilder()
                 .when(goodsEntity.categoryEntity.categoryNo.eq(1L).and(goodsEntity.price.between(0L, 1000000L))).then("1-1")
                 .when(goodsEntity.categoryEntity.categoryNo.eq(1L).and(goodsEntity.price.between(1000000L, 2000000L))).then("1-2")
@@ -117,8 +96,6 @@ class GoodsGroupByTest {
 
     @Test
     void subQueryAtFrom() {
-        // 일시불: 카테고리, 가격 범위
-        makeGoodsTabSearch();
 
         StringExpression caseBuilder = new CaseBuilder()
                 .when(goodsEntity.categoryEntity.categoryNo.eq(1L).and(goodsEntity.price.between(0L, 1000000L))).then("1-1")
@@ -141,7 +118,24 @@ class GoodsGroupByTest {
     // JPA 유지 하고 N 번 쿼리 날릴지, Mybatis 추가하고 원쿼리로 해결할지 고민 필요
     @Test
     void getTabProductCountsMultipleQuery() {
-        GoodsTabSearch goodsTabSearch = makeGoodsTabSearch();
+        GoodsTabSearch goodsTabSearch = new GoodsTabSearch();
+        goodsTabSearch.setTabSearchType(TabSearchType.LUMP_SUM_PAY_GOODS.getCode());
+        goodsTabSearch.setGoodsTabList(List.of(
+                GoodsTab.of("1", 1L, null, null, null),
+                GoodsTab.of("1-1", 1L, 0L, 1000000L, null),
+                GoodsTab.of("1-2", 1L, 1000000L, 2000000L, null),
+                GoodsTab.of("1-3", 1L, 2000000L, null, null),
+
+                GoodsTab.of("2", 2L, null, null, null),
+                GoodsTab.of("2-1", 2L, 0L, 1000000L, null),
+                GoodsTab.of("2-2", 2L, 1000000L, 2000000L, null),
+                GoodsTab.of("2-3", 2L, 2000000L, null, null),
+
+                GoodsTab.of("3", 3L, null, null, null),
+                GoodsTab.of("3-1", 3L, 0L, 1000000L, null),
+                GoodsTab.of("3-2", 3L, 1000000L, 2000000L, null),
+                GoodsTab.of("3-3", 3L, 2000000L, null, null)
+        ));
 
         // 동적 쿼리 빌더
         BooleanBuilder builder = new BooleanBuilder();
@@ -171,6 +165,71 @@ class GoodsGroupByTest {
         log.info("finish: {}", goodsCountDtos);
     }
 
+    @Test
+    void getTabProductCountsSingleQuery() {
+        GoodsTabSearch goodsTabSearch = new GoodsTabSearch();
+        goodsTabSearch.setTabSearchType(TabSearchType.LUMP_SUM_PAY_GOODS.getCode());
+        goodsTabSearch.setGoodsTabList(List.of(
+                GoodsTab.of("1-1", 1L, 0L, 1000000L, null),
+                GoodsTab.of("1-2", 1L, 1000000L, 2000000L, null),
+                GoodsTab.of("1-3", 1L, 2000000L, null, null),
+
+                GoodsTab.of("2-1", 2L, 0L, 1000000L, null),
+                GoodsTab.of("2-2", 2L, 1000000L, 2000000L, null),
+                GoodsTab.of("2-3", 2L, 2000000L, null, null),
+
+                GoodsTab.of("3-1", 3L, 0L, 1000000L, null),
+                GoodsTab.of("3-2", 3L, 1000000L, 2000000L, null),
+                GoodsTab.of("3-3", 3L, 2000000L, null, null)
+        ));
+
+        List<GoodsTab> tabConditions = goodsTabSearch.getGoodsTabList();
+        BooleanExpression trueExpression = goodsEntity.goodsNo.eq(goodsEntity.goodsNo);
+        CaseBuilder.Cases<String, StringExpression> caseExpression = null;
+        for (GoodsTab condition : tabConditions) {
+            BooleanExpression tabCondition = trueExpression;
+
+            BooleanExpression categoryCondition = getCategoryNo(condition.getCategoryNo());
+            if (categoryCondition != null) {
+                tabCondition = tabCondition.and(categoryCondition);
+            }
+
+            BooleanExpression priceCondition = getPriceCondition(condition.getMinPrice(), condition.getMaxPrice());
+            if (priceCondition != null) {
+                tabCondition = tabCondition.and(priceCondition);
+            }
+
+            BooleanExpression installmentCondition = getInstallmentCondition(condition.getInstallmentMonth());
+            if (installmentCondition != null) {
+                tabCondition = tabCondition.and(installmentCondition);
+            }
+
+            if (caseExpression == null) {
+                caseExpression = new CaseBuilder().when(tabCondition).then(condition.getTabId());
+            } else {
+                caseExpression = caseExpression.when(tabCondition).then(condition.getTabId());
+            }
+        }
+        if (caseExpression == null) {
+            caseExpression = new CaseBuilder().when(trueExpression).then("default");
+        }
+
+        StringExpression caseTabIcd = caseExpression.otherwise("none");
+
+        BooleanExpression baseCondition = goodsEntity.isDisplay.isTrue()
+                .and(goodsEntity.isLumpSumPayGoods.isTrue());
+        List<GoodsCountDto> goodsCountDtos = queryFactory
+                .select(Projections.fields(GoodsCountDto.class,
+                        caseTabIcd.as("tabId"),
+                        goodsEntity.count().as("count"))) // count 추가
+                .from(goodsEntity)
+                .where(baseCondition)
+                .groupBy(caseTabIcd)
+                .fetch();
+
+        log.info("finish: {}", goodsCountDtos);
+    }
+
     private BooleanExpression getCategoryNo(Long categoryNo) {
         if (categoryNo != null) {
             return goodsEntity.categoryEntity.categoryNo.eq(categoryNo);
@@ -196,65 +255,6 @@ class GoodsGroupByTest {
             return null;
         }
         return goodsEntity.installmentMonth.eq(installmentMonths);
-    }
-
-    // Cannot invoke "com.querydsl.core.types.Expression.accept(com.querydsl.core.types.Visitor, Object)" because "arg" is null 발생
-    @Test
-    void getTabProductCountsSingleQuery() {
-        GoodsTabSearch goodsTabSearch = makeGoodsTabSearch();
-
-        List<GoodsTab> tabConditions = goodsTabSearch.getGoodsTabList();
-
-        BooleanExpression baseCondition = goodsEntity.isDisplay.isTrue()
-                .and(goodsEntity.isLumpSumPayGoods.isTrue());
-
-        CaseBuilder caseBuilder = new CaseBuilder();
-        CaseBuilder.Cases<String, StringExpression> caseExpression = null;
-
-        if (tabConditions.isEmpty()) {
-            // 탭 조건이 없는 경우 기본 조건만으로 처리
-            caseExpression = caseBuilder.when(baseCondition).then("default");
-        } else {
-            for (GoodsTab condition : tabConditions) {
-                BooleanExpression tabCondition = baseCondition; // 기본 조건 포함
-
-                BooleanExpression categoryCondition = getCategoryNo(condition.getCategoryNo());
-                if (categoryCondition != null) {
-                    tabCondition = tabCondition.and(categoryCondition);
-                }
-
-                BooleanExpression priceCondition = getPriceCondition(condition.getMinPrice(), condition.getMaxPrice());
-                if (priceCondition != null) {
-                    tabCondition = tabCondition.and(priceCondition);
-                }
-
-                BooleanExpression installmentCondition = getInstallmentCondition(condition.getInstallmentMonth());
-                if (installmentCondition != null) {
-                    tabCondition = tabCondition.and(installmentCondition);
-                }
-
-                if (caseExpression == null) {
-                    caseExpression = caseBuilder.when(tabCondition).then(condition.getTabId());
-                } else {
-                    caseExpression = caseExpression.when(tabCondition).then(condition.getTabId());
-                }
-            }
-        }
-
-        // caseExpression이 여전히 null인 경우 (모든 조건이 null을 반환한 경우)
-        if (caseExpression == null) {
-            caseExpression = caseBuilder.when(baseCondition).then("default");
-        }
-
-        List<GoodsCountDto> goodsCountDtos = queryFactory
-                .select(Projections.fields(GoodsCountDto.class,
-                        caseExpression.otherwise("none").as("tabId"),
-                        goodsEntity.count().as("count"))) // count 추가
-                .from(goodsEntity)
-                .where(baseCondition)
-                .groupBy(caseExpression.otherwise("none"))
-                .fetch();
-        log.info("finish: {}", goodsCountDtos);
     }
 
 
