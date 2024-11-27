c12e5d6d99dd66ee002e670670ce867cf6c3fce3
박현준 <hyeonj1120@plateer.com>
2024-11-27 13:19:29 +0900

    KeepAlive 테스트

---
+++ front/components/corner/CompB.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+onMounted(() =>{
+  console.log('omMounted CompB')
+})
+
+</script>
+
+<template>
+  <p>Current component: B</p>
+  <span>count: {{ count }}</span>
+  <button @click="count++">+</button>
+</template>
